7b9f998cf8d239322135f505d0b2ca3551f5ac14
rafabcanedo <rafaitecanedo@gmail.com>
2025-04-26 09:35:20 -0300

    feat:add and customise validations

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -2,21 +2,26 @@ package controller
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/gin-gonic/gin"
 	"github.com/rafabcanedo/api-golang/src/configuration/rest_errors"
+	"github.com/rafabcanedo/api-golang/src/configuration/validation"
 	"github.com/rafabcanedo/api-golang/src/controller/model/request"
 )
 
 func CreateUser(c *gin.Context) {
 
+	log.Println("Init CreateUser controller")
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
 		restError := rest_errors.NewBadRequestError(
 			fmt.Sprintf("There are some incorrect filds, error=%s\n", err.Error()))
 
-			c.JSON(restError.Code, restError)
+			errRest := validation.ValidateUserError(err)
+
+			c.JSON(restError.Code, errRest)
 
 			return
 	}
--- src/controller/model/request/user_request.go
+++ src/controller/model/request/user_request.go
@@ -1,8 +1,8 @@
 package request
 
 type UserRequest struct {
-	Name string `json:"name"`
-	Email string `json:"email"`
-	Password string `json:"password"`
-	Age int8 `json:"age"`
+	Name string `json:"name" binding:"required,min=4,max=20"`
+	Email string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=6,containsany=!@#S%*"`
+	Age int8 `json:"age" binding:"required,numeric,min=1,max=100"`
 }
